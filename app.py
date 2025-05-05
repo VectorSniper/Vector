475d1bc75a9dc26c0783766ed412dea4c2e1ed17
VectorSniper <apostareganhar@gmail.com>
2025-05-06 00:47:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Vector online"
+    return "Vector online com sucesso!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
-  
